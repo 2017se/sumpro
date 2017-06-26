602473c76bb4d0cd000e85e780a96d1ef32103d2
lishuaiqing <438596326@qq.com>
2017-06-26 14:58:23 +0800

    modle
    the begin of our pro & the 1.0 modle // by LSQ

---
+++ sepro/src/model/ch_tab.java
@@ -0,0 +1,26 @@
+package model;
+
+public class ch_tab {
+	private int selectID;
+	private char questionID;
+	private char Scontent;
+	public int getSelectID() {
+		return selectID;
+	}
+	public void setSelectID(int selectID) {
+		this.selectID = selectID;
+	}
+	public char getQuestionID() {
+		return questionID;
+	}
+	public void setQuestionID(char questionID) {
+		this.questionID = questionID;
+	}
+	public char getScontent() {
+		return Scontent;
+	}
+	public void setScontent(char scontent) {
+		Scontent = scontent;
+	}
+	
+}
