bf9a5d2facf2a97f563bda34a070665cad6e3458
lishuaiqing <438596326@qq.com>
2017-06-26 15:15:16 +0800

    modle 1.01
    change some fault and uncommited //By LSQ

---
+++ sepro/src/model/answersheet.java
@@ -0,0 +1,26 @@
+package model;
+
+public class answersheet {
+		private int ID;
+		private int userID;
+		private int selectID;
+		public int getID() {
+			return ID;
+		}
+		public void setID(int iD) {
+			ID = iD;
+		}
+		public int getUserID() {
+			return userID;
+		}
+		public void setUserID(int userID) {
+			this.userID = userID;
+		}
+		public int getSelectID() {
+			return selectID;
+		}
+		public void setSelectID(int selectID) {
+			this.selectID = selectID;
+		}
+		
+}
--- sepro/src/model/questiontype.java
+++ sepro/src/model/questiontype.java
@@ -1,5 +1,19 @@
 package model;
 
 public class questiontype {
-
+  private char topicID;
+  private char typeName;
+public char getTopicID() {
+	return topicID;
+}
+public void setTopicID(char topicID) {
+	this.topicID = topicID;
+}
+public char getTypeName() {
+	return typeName;
+}
+public void setTypeName(char typeName) {
+	this.typeName = typeName;
+}
+  
 }
